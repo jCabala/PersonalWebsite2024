37db9426ac89efee02caf37109be4c5e5dd8b7e6
Jakub Cabala <kuba.cabala6@gmail.com>
2024-02-23 13:07:06 +0000

    fix: Removed a comment from the header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,6 +1,5 @@
 import siteMetadata from '@/data/siteMetadata'
 import headerNavLinks from '@/data/headerNavLinks'
-import Logo from '@/data/logo.svg'
 import Link from './Link'
 import MobileNav from './MobileNav'
 import ThemeSwitch from './ThemeSwitch'
@@ -12,11 +11,6 @@ const Header = () => {
       <div>
         <Link href="/" aria-label={siteMetadata.headerTitle}>
           <div className="flex items-center justify-between">
-              {/* <div className="mr-3">
-                <Logo />
-                </div> */}
-              {/* If you want a logo add a data/logo.svg file and uncomment this tag
-              and the import above */}
             {typeof siteMetadata.headerTitle === 'string' ? (
               <div className="hidden h-6 text-2xl font-semibold sm:block">
                 {siteMetadata.headerTitle}
