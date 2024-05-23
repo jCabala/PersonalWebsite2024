ad0241f7d61f6724b88c73517f3dd89529a380bd
Jakub Cabala <kuba.cabala6@gmail.com>
2024-05-23 23:05:24 +0100

    Added placeholders for projects

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -21,6 +21,14 @@ const universityProjects: ProjectGroup = {
       imgSrc: '/static/images/pintos.jpg',
       href: 'https://www.doc.ic.ac.uk/~mjw03/OSLab/pintos.pdf',
     },
+    {
+      title: 'WACC',
+      description: `What if you could look up any information in the world? Webpages, images, videos
+    and more. Google has many features to help you find exactly what you're looking
+    for.`,
+      imgSrc: '/static/images/pintos.jpg',
+      href: 'https://www.doc.ic.ac.uk/~mjw03/OSLab/pintos.pdf',
+    },
   ],
 }
 
